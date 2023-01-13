bbe61d48ec849f0bfdaa2c67f172361ba6840025
Alexander Vazquez <Alexander Vazquez@LAPTOP-RFKUOQAP>
2023-01-13 02:29:09 -0500

    Agregar archivos de proyecto.

---
+++ bloquesExcepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloquesExcepciones
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            bool comprueba = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Ingrese un valor:");
+                    string linea = Console.ReadLine();
+                    var num = int.Parse(linea);
+                    var cuadrado = num * num;
+                    Console.WriteLine($"El cuadrado de {num} es {cuadrado}");
+                    comprueba = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Debe ingresar un entero");
+                }
+            } while (comprueba == false);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
